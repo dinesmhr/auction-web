b0fbe66ccd927671a3b2e5c4e91ac22ae23edee6
dineshmhr <dinesh.mhr2054@gmail.com>
2020-08-20 21:38:23 +0545

    database connection code done

--- backend/connection.php
+++ backend/connection.php
@@ -5,3 +5,15 @@
  * @package Auction Web
  * @since 1.0.0
  */
+$hostname = 'localhost';
+$database = 'auction-web';
+$username = 'root';
+$password = '';
+
+$mysqli = new mysqli( $hostname, $username, $password, $database );
+if( $mysqli->connect_errno ) {
+    echo 'Connection Error!! Cannot connect to the  database';
+    exit;
+} else {
+    echo 'Connection established successfully!!';
+}
