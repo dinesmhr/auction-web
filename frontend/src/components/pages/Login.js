03f340c8d2bac4920a3d2f3524c4fa3122bee43b
dineshmhr <dinesh.mhr2054@gmail.com>
2020-09-04 13:07:19 +0545

    login form update

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            username: '',
+            password: ''
+        }
     }
+
+    setInputValueChange( key, val ) {
+        this.setState({
+            [key] : val
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault()
+    }
+
     render() { 
+        const { username, password } = this.state
         return ( 
             <div id="auction-web-login" className="page--login main-wrapper">
                 <form id="aweb-login-form">
                     <div className="aweb-username">
                         <label>Username</label>
-                        <input type="text" name="username"/>
+                        <input type="text" name="username" onChange={ (e) => this.setInputValueChange( 'username', e.target.value) } defaultValue={ username }/>  
                     </div>
                     <div className="aweb-password">
                         <label>Password</label>
-                        <input type="password" name="password"/>
+                        <input type="password" name="password" onChange={ (e) => this.setInputValueChange( 'password', e.target.value) } defaultValue={ password }/>
                     </div>
                     <div className="aweb-submit">
-                        <input type="submit" name="submit" value="Login"/>
+                        <input type="submit" name="submit" onClick= { (e) => this.onSubmit(e) } value="Submit"/>
                     </div>
                 </form>
             </div>
