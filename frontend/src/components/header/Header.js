40f64178bb6ccc49ba30ef309b3c7dd870c2b756
eparis@atlassian.com <dinesh.mhr2054@gmail.com>
2020-10-18 23:34:53 +0545

    intial setup pull

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import AdminNavigation from '../navigation/AdminNavigation'
 import MainNavigation from '../navigation/MainNavigation'
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+    }
     render() { 
-        return ( 
-            <header id="aweb-header">
-                <h1 className="aweb-site-title">Auction Web</h1>
-                <MainNavigation />
+        const { userLoggedIn } = this.props
+        return (
+            <header>
+                { userLoggedIn &&
+                    <AdminNavigation />
+                }
+                <div className="aweb-header">
+                    <h1 className="aweb-site-title"><a href="/">Auction Web</a></h1>
+                    <MainNavigation userLoggedIn = { userLoggedIn } />
+                </div>
             </header>
         );
     }
