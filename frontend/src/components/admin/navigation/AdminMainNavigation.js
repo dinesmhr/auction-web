df51c5497b9a29ea556e453b922466e076d6559a
nehashakya555 <nehashakya555@gmail.com>
2020-10-19 16:16:30 +0545

    user table error

--- frontend/src/components/admin/navigation/AdminMainNavigation.js
+++ frontend/src/components/admin/navigation/AdminMainNavigation.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+
 
 class AdminMainNavigation extends Component {
     render() { 
         return ( 
             <nav id="aweb-admin-front-navigation">
-                 <ul className="admin-front-nav-wrap">
-                 	<Tabs>
-                        <NavLink to="/aweb-admin"><li><Tab label="Dashboard"/></li></NavLink>
-                        <NavLink to="/aweb-users"><li><Tab label="Users"/></li></NavLink>
-                  	</Tabs>
+                 <ul className="admin-front-nav-wrap">     
+                        <NavLink to="/aweb-admin"><li>Dashboard</li></NavLink>
+                        <NavLink to="/aweb-users"><li>Users</li></NavLink>                  	
                  </ul>
             </nav>
         );
--- frontend/src/components/admin/pages/users.js
+++ frontend/src/components/admin/pages/users.js
@@ -1,22 +1,66 @@
-import React, { Component } from 'react';
+  
+import React, { Component, useEffect, useState } from 'react';
 import AdminMainNavigation from '../navigation/AdminMainNavigation'
+import axios from 'axios';
 
-class AdminUsers extends Component {
-    constructor(props) {
-        super(props)
-    }
 
-    render() { 
-        const { users } = this.props
-        return (
-            <header>           
+const AdminUsers =(props) =>{
+
+
+	const [id, setID] = useState();
+	const [username, setUsername] = useState();
+	const [password, setPassword] = useState();
+	const [email, setEmail] = useState();
+
+
+	useEffect(()=>{
+		async function getData() {
+			const res = await axios.get(`http://localhost/auction-web/api/users.php`) 
+		
+
+		//	setID(res.data.data[0].id);
+		//	setUsername(res.data.data[0].username);
+		//	setPassword(res.data.data[0].password);
+		//	setEmail(res.data.data[0].email);
+		}
+		getData();
+	})
+
+
+	
+
+	return (
+		<>
+		 <header>           
                 <div className="aweb-admin-top-header">
                     <h1 className="aweb-admin-site-title">Auction Web</h1>
-                    <AdminMainNavigation userLoggedIn = {this.props.userLoggedIn}/>
+                    <AdminMainNavigation userLoggedIn = {props.userLoggedIn}/> 
+                    	<table style={{width:"100%"}}>
+                    		<thead>
+								  <tr>
+								    <th>ID</th>
+								    <th>Username</th>
+								    <th>Password</th>
+								    <th>Email</th>
+								  </tr>
+							</thead>
+							<tbody>	  
+								  <tr>
+								    <td>{id}</td>
+								    <td>S{username}</td>
+								    <td>{password}</td>
+								    <td>{email}</td>
+								  </tr>								 
+							</tbody>	  
+					</table>					
                 </div>
             </header>
-        );
-    }
+		</>
+
+		)
+
+
 }
- 
+
+
 export default AdminUsers;
