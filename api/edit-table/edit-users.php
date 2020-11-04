fe8c3bb1df62e71e6a9872172a79496ac904c5b0
dineshmhr <dinesh.mhr2054@gmail.com>
2020-11-05 04:11:46 +0545

    edit users api update

--- api/edit-table/edit-users.php
+++ api/edit-table/edit-users.php
@@ -4,3 +4,18 @@
  * 
  * @package Auction Web
  */
+
+header("Access-Control-Allow-Origin: *");
+require_once 'functions.php';
+if( is_db_connected() ) {
+    $fullname = $_POST['fullname'];
+    $username = $_POST['username'];
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+    $role     = isset( $_POST['role'] ) ? $_POST['role'] : 'subscriber';
+    $status   = isset( $_POST['status'] ) ? $_POST['status'] : 'nnot-verified';
+} else {
+    $structure['status'] = false;
+    $structure['message'] = 'error';
+}
+echo json_encode( $structure );
