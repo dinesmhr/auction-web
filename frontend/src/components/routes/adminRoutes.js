b4c61452310d08dc04a91916385544e128b59630
dineshmhr <dinesh.mhr2054@gmail.com>
2021-06-17 17:42:48 +0545

    modify

--- frontend/src/components/routes/adminRoutes.js
+++ frontend/src/components/routes/adminRoutes.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import AdminDashboard from '../admin/pages/dashboard'
 import AdminUsers from '../admin/pages/users'
 import AdminProducts from '../admin/pages/products'
-import AdminEditUser from '../admin/pages/editUsers'
-import AdminEditProduct from '../admin/pages/editProducts'
 import AdminProductCategories from '../admin/pages/product-categories'
 import AdminProductTags from '../admin/pages/product-tags'
+
+import AdminEditUser from '../admin/pages/edit-page/editUsers'
+import AdminEditProduct from '../admin/pages/edit-page/editProducts'
 
 const axios = require('axios')
 
@@ -44,10 +45,13 @@ export const AdminRoutes = (props) => {
                 <Route path="/aweb-admin" component={() => <AdminDashboard/>}></Route>
                 <Route path="/aweb-users" exact component={() => <AdminUsers/>}></Route>
                 <Route path="/aweb-products" exact component={() => <AdminProducts/>}></Route>
-                <Route path="/aweb-users/:id" component={(props) => <AdminEditUser/>}></Route>
-                <Route path="/aweb-products/:id" component={(props) => <AdminEditProduct/>}></Route>
                 <Route path="/aweb-categories" exact component={() => <AdminProductCategories/>}></Route>
                 <Route path="/aweb-tags" exact component={() => <AdminProductTags/>}></Route>
+                { 
+                    // Single root
+                }
+                <Route path="/aweb-users/:id" component={(props) => <AdminEditUser/>}></Route>
+                <Route path="/aweb-products/:id" component={(props) => <AdminEditProduct/>}></Route>
             </Switch>
         </BrowserRouter>
     )
