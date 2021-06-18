6ab6eb2e185d337675f23f01dc50293adfa90165
dineshmhr <dinesh.mhr2054@gmail.com>
2021-06-18 16:21:34 +0545

    updates

--- frontend/src/components/admin/pages/edit-page/editUsers.js
+++ frontend/src/components/admin/pages/edit-page/editUsers.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import AdminMainNavigation from '../../navigation/AdminMainNavigation'
+import ModalImage from "react-modal-image";
 
 const axios = require('axios');
 
@@ -14,9 +15,11 @@ const AdminEditUser = () =>  {
         .then(function(res) {
             if( res.data.status ) {
                 setUser( res.data.data )
+                setUserStatus(res.data.data[0].status)
             }
         })
     })
+    console.log(user)
 
     const verifyUser = () => {
 
@@ -33,18 +36,8 @@ const AdminEditUser = () =>  {
                                 <div key={ index } >
                                     { element.fullname &&
                                         <div className="admin-single-user-field">
-                                            <strong>Fullname :</strong> 
+                                            <strong>Fullname :</strong>
                                             { element.fullname }
-                                        </div>
-                                    }
-                                    { element.username &&
-                                        <div className="admin-single-user-field">
-                                            <strong>Username :</strong> { element.username }
-                                        </div>   
-                                    }
-                                    { element.birth_date &&
-                                        <div className="admin-single-user-field">
-                                            <strong>Birth Date :</strong> { element.birth_date }
                                         </div>
                                     }
                                     { element.email &&
@@ -52,34 +45,77 @@ const AdminEditUser = () =>  {
                                             <strong>Email Address :</strong> { element.email }
                                         </div>
                                     }
-                                    { element.parent_name &&
+                                    { element.username &&
                                         <div className="admin-single-user-field">
-                                            <strong>Guardian's Name :</strong> { element.parent_name }
+                                            <strong>Username :</strong> { element.username }
+                                        </div>   
+                                    }
+                                    { element.birthdate &&
+                                        <div className="admin-single-user-field">
+                                            <strong>Birth Date :</strong>
+                                            <span> Day: { element.birthdate.day }</span>
+                                            <span> Month: { element.birthdate.month }</span>
+                                            <span> Year: { element.birthdate.year }</span>
                                         </div>
                                     }
-                                    { element.profession &&
+                                    { element.contact_num &&
                                         <div className="admin-single-user-field">
-                                            <strong>Profession :</strong> { element.profession }
+                                            <strong>Contact Number :</strong> { element.contact_num.areaCode + ' ' + element.contact_num.number }
                                         </div>
                                     }
-                                    { element.contact_number &&
+                                    { element.status &&
                                         <div className="admin-single-user-field">
-                                            <strong>Contact Number :</strong> { element.contact_number }
+                                            <strong>Status :</strong> { element.status }
                                         </div>
                                     }
-                                    { element.current_address &&
+                                    { element.permanent_ad &&
                                         <div className="admin-single-user-field">
-                                            <strong>Current Address :</strong> { element.current_address }
+                                            <div>Current Address </div>
+                                            <div>
+                                                Street Address:
+                                                { element.current_ad.streetAddress }
+                                            </div>
+                                            <div>
+                                                Postal Code:
+                                                { element.current_ad.postalCode }
+                                            </div>
+                                            <div>
+                                                State/Province:
+                                                { element.current_ad.stateProvince }
+                                            </div>
+                                            <div>
+                                                City:
+                                                { element.current_ad.city }
+                                            </div>
+                                            <div>
+                                                Country:
+                                                { element.current_ad.country }
+                                            </div>
                                         </div>
                                     }
-                                    { element.permanent_address &&
+                                    { element.permanent_ad &&
                                         <div className="admin-single-user-field">
-                                            <strong>Permanent Address :</strong> { element.permanent_address }
-                                        </div>
-                                    }
-                                    { element.document_type &&
-                                        <div className="admin-single-user-field">
-                                            <strong>Document Submitted :</strong> { element.document_type }
+                                            <div>Permanent Address </div>
+                                            <div>
+                                                Street Address:
+                                                { element.permanent_ad.streetAddress }
+                                            </div>
+                                            <div>
+                                                Postal Code:
+                                                { element.permanent_ad.postalCode }
+                                            </div>
+                                            <div>
+                                                State/Province:
+                                                { element.permanent_ad.stateProvince }
+                                            </div>
+                                            <div>
+                                                City:
+                                                { element.permanent_ad.city }
+                                            </div>
+                                            <div>
+                                                Country:
+                                                { element.permanent_ad.country }
+                                            </div>
                                         </div>
                                     }
                                     { element.role &&
@@ -87,40 +123,59 @@ const AdminEditUser = () =>  {
                                             <strong>Role :</strong> { element.role }
                                         </div>
                                     }
+                                    { element.document_type &&
+                                        <div className="admin-single-user-field">
+                                            <strong>Document Submitted :</strong> { element.document_type }
+                                        </div>
+                                    }
                                     { element.pphoto &&
                                         <div className="admin-single-user-field">
                                             <strong>Personal Photo :</strong> <img src={ element.pphoto } />
                                         </div>
                                     }
-                                    { element.document_image_one &&
+                                    { element.document_image_path &&
                                         <div className="admin-single-user-field">
-                                            <strong>Document Image One :</strong> <img src={ element.document_image_one } />
+                                            <strong>Document Image One :</strong> 
+                                            <div>
+                                                <ModalImage
+                                                    small={ `http://localhost/auction-web/${element.document_image_path.split('../').pop()}` }
+                                                    large={ `http://localhost/auction-web/${element.document_image_path.split('../').pop()}` }
+                                                    hideDownload= {true}
+                                                    showRotate={true}
+                                                />
+                                            </div>
                                         </div>
                                     }
-                                    { element.document_image_two &&
+                                    { element.document_image_two_path &&
                                         <div className="admin-single-user-field">
-                                            <strong>Document Image Two :</strong> <img src={ element.document_image_two } />
+                                            <strong>Document Image Two :</strong> 
+                                            <div>
+                                                <ModalImage
+                                                    small={ `http://localhost/auction-web/${element.document_image_two_path.split('../').pop()}` }
+                                                    large={ `http://localhost/auction-web/${element.document_image_two_path.split('../').pop()}` }
+                                                    hideDownload= {true}
+                                                    showRotate={true}
+                                                />
+                                            </div>
                                         </div>
                                     }
                                 </div>
                             )
                         })
                     }
+                    <div>
+                        { ( userStatus === 'under-verification' ) &&
+                                <button onClick = { (e) => verifyUser() }>Verify the user</button>
+                        }
+                        { ( userStatus === 'not-verified' ) &&
+                                <button>Document not available for verification</button>
+                        }
+                        { ( userStatus === 'verified' ) &&
+                                <button>Verified User</button>
+                        }
+                        <button>Delete the User</button>
+                    </div>
                 </div>
-            </div>
-            <div className="aweb-single-user-edit">
-                <form id="admin-edit-users-form">
-                </form>
-                { ( userStatus === 'under-verification' ) &&
-                        <button onClick = { (e) => verifyUser() }>Verify the user</button>
-                }
-                { ( userStatus === 'not-verified' ) &&
-                        <button>Document not available for verification</button>
-                }
-                { ( userStatus === 'verified' ) &&
-                        <button>Verified User</button>
-                }
-                <button>Delete the User</button>
             </div>
         </>
     )
