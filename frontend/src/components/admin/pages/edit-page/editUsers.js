60812d1bf99a37cfd5f76041755c3669897c3ad5
nehashakya555 <nehashakya555@gmail.com>
2021-06-18 19:53:31 +0545

    user edit css

--- frontend/src/components/admin/pages/edit-page/editUsers.js
+++ frontend/src/components/admin/pages/edit-page/editUsers.js
@@ -36,96 +36,139 @@ const AdminEditUser = () =>  {
                                 <div key={ index } >
                                     { element.fullname &&
                                         <div className="admin-single-user-field">
-                                            <strong>Fullname :</strong>
+                                            <strong>Fullname: </strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                             { element.fullname }
+                                            </div>
                                         </div>
                                     }
                                     { element.email &&
                                         <div className="admin-single-user-field">
-                                            <strong>Email Address :</strong> { element.email }
+                                            <strong>Email Address:</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                             { element.email }
+                                            </div>
                                         </div>
                                     }
                                     { element.username &&
-                                        <div className="admin-single-user-field">
-                                            <strong>Username :</strong> { element.username }
+                                        <div className="admin-single-user-field">                                      
+                                            <strong>Username :</strong> 
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                            { element.username }
+                                            </div>
                                         </div>   
                                     }
                                     { element.birthdate &&
                                         <div className="admin-single-user-field">
                                             <strong>Birth Date :</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+
                                             <span> Day: { element.birthdate.day }</span>
                                             <span> Month: { element.birthdate.month }</span>
                                             <span> Year: { element.birthdate.year }</span>
                                         </div>
+                                        </div>
                                     }
                                     { element.contact_num &&
                                         <div className="admin-single-user-field">
-                                            <strong>Contact Number :</strong> { element.contact_num.areaCode + ' ' + element.contact_num.number }
+                                            <strong>Contact Number :</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                             { element.contact_num.areaCode + ' ' + element.contact_num.number }
+                                             </div>
                                         </div>
                                     }
                                     { element.status &&
                                         <div className="admin-single-user-field">
-                                            <strong>Status :</strong> { element.status }
+                                            <strong>Status :</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                             { element.status }
+                                             </div>
                                         </div>
                                     }
                                     { element.permanent_ad &&
                                         <div className="admin-single-user-field">
-                                            <div>Current Address </div>
+                                            <div className="flex justify-center mt-3 italic">Current Address </div>
                                             <div>
                                                 Street Address:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.current_ad.streetAddress }
+                                                </div>
                                             </div>
-                                            <div>
+                                            <div>                                           
                                                 Postal Code:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.current_ad.postalCode }
+                                                </div>
                                             </div>
                                             <div>
                                                 State/Province:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.current_ad.stateProvince }
+                                                </div>
                                             </div>
                                             <div>
                                                 City:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.current_ad.city }
+                                                </div>
                                             </div>
                                             <div>
                                                 Country:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.current_ad.country }
+                                                </div>
                                             </div>
                                         </div>
                                     }
                                     { element.permanent_ad &&
                                         <div className="admin-single-user-field">
-                                            <div>Permanent Address </div>
+                                            <div className="flex justify-center mt-3 italic">Permanent Address </div>
                                             <div>
                                                 Street Address:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.permanent_ad.streetAddress }
+                                                </div>
                                             </div>
                                             <div>
                                                 Postal Code:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.permanent_ad.postalCode }
+                                                </div>
                                             </div>
                                             <div>
                                                 State/Province:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.permanent_ad.stateProvince }
+                                                </div>
                                             </div>
                                             <div>
                                                 City:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.permanent_ad.city }
+                                                </div>
                                             </div>
                                             <div>
                                                 Country:
+                                                <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
                                                 { element.permanent_ad.country }
+                                                </div>
                                             </div>
                                         </div>
                                     }
                                     { element.role &&
                                         <div className="admin-single-user-field">
-                                            <strong>Role :</strong> { element.role }
+                                            <strong>Role :</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                             { element.role }
+                                            </div> 
                                         </div>
                                     }
                                     { element.document_type &&
                                         <div className="admin-single-user-field">
-                                            <strong>Document Submitted :</strong> { element.document_type }
+                                            <strong>Document Submitted :</strong>
+                                            <div className="input_editUser bg-gray-700 appearance-none border-1 border-gray-200 rounded mb-3 w-full py-1 px-5 ml-2 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">
+                                             { element.document_type }
+                                            </div>
                                         </div>
                                     }
                                     { element.pphoto &&
@@ -134,9 +177,9 @@ const AdminEditUser = () =>  {
                                         </div>
                                     }
                                     { element.document_image_path &&
-                                        <div className="admin-single-user-field">
+                                        <div className="admin-single-user-field w-48 h-auto mt-3">
                                             <strong>Document Image One :</strong> 
-                                            <div>
+                                            <div className="mt-2">
                                                 <ModalImage
                                                     small={ `http://localhost/auction-web/${element.document_image_path.split('../').pop()}` }
                                                     large={ `http://localhost/auction-web/${element.document_image_path.split('../').pop()}` }
@@ -147,9 +190,9 @@ const AdminEditUser = () =>  {
                                         </div>
                                     }
                                     { element.document_image_two_path &&
-                                        <div className="admin-single-user-field">
+                                        <div className="admin-single-user-field w-48 h-auto mt-3 mb-4">
                                             <strong>Document Image Two :</strong> 
-                                            <div>
+                                            <div className="mt-2">
                                                 <ModalImage
                                                     small={ `http://localhost/auction-web/${element.document_image_two_path.split('../').pop()}` }
                                                     large={ `http://localhost/auction-web/${element.document_image_two_path.split('../').pop()}` }
@@ -171,9 +214,9 @@ const AdminEditUser = () =>  {
                                 <button>Document not available for verification</button>
                         }
                         { ( userStatus === 'verified' ) &&
-                                <button>Verified User</button>
+                                <button className="bg-white hover:bg-gray-300 text-gray-800 font-semibold py-1 px-2 mr-3 border border-gray-400 rounded shadow">Verified User</button>
                         }
-                        <button>Delete the User</button>
+                        <button className="bg-red-600 hover:bg-red-700 text-gray-900 font-semibold py-1 px-2 border border-gray-400 shadow-md rounded shadow">Delete the User</button>
                     </div>
                 </div>
             </div>
