3a207f3d91822ba62f5bc1c9388664e7b1f9a068
dineshmhr <dinesh.mhr2054@gmail.com>
2021-06-19 21:43:21 +0545

    updates

--- frontend/src/components/admin/pages/edit-page/editProductCategory.js
+++ frontend/src/components/admin/pages/edit-page/editProductCategory.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import AdminMainNavigation from '../../navigation/AdminMainNavigation'
 
 const AdminEditProductCategory = () => {
+    const { id } = useParams()
+
     return (
         <>
-            This is Product edit page
+            <div className="content-wrap">
+				<AdminMainNavigation/>
+				<div id="admin-right-content" className="float-right w-4/5 text-white p-8 h-screen mt-12">
+
+                </div>{ /* #admin-right-content */ }
+            </div>
         </>
     )
 }
