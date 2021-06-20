27290bbcf761e9ddcb82955cb7093b2417efcadc
nehashakya555 <nehashakya555@gmail.com>
2021-06-20 16:40:38 +0545

    fetch

--- frontend/src/components/pages/single-page/singleProduct.js
+++ frontend/src/components/pages/single-page/singleProduct.js
@@ -5,7 +5,7 @@ import Header from '../../header/Header'
 const axios = require('axios')
 
 const SingleProduct = (props) => {
-    const [productData, setProductData] = useState()
+    const [productData, setProductData] = useState(null)
     const { id } = useParams()
     const { isLoggedIn } = props
 
@@ -20,10 +20,62 @@ const SingleProduct = (props) => {
     })
 
     return (
+        <>  
         <div id="auction-web">
-            <Header isLoggedIn = { isLoggedIn } />
-            This is Single Product page
+            <Header isLoggedIn = { isLoggedIn } />                    
+            <div id="auction-web-singlePage" className="">
+
+                    {
+                        productData === null ? (
+                            <div id="singlePage" className="tracking-wider"><span className="">Loading datas..</span></div>
+                        ) : productData.length === 0 ? (
+                            <div id="singlePage" className="tracking-wider"><span className="">No products found</span></div>
+                        ) : (
+                            <div id="singlePage flex flex-row" className="tracking-wider">
+                                <div className="singlePage m-10 flex flex-col">
+                                    <span className="">Title</span>
+                                    <span className="">Category</span>
+                                    <span className="">Tags</span>
+                                    <span className="">Description</span>
+                                    <span className="">Submission Date</span>
+                                    <span className="">Deadline Date</span>
+                                    <span className="">Initial bid</span>
+                                    <span className="">Specification</span>
+                                    <span className="">Images</span>
+                                </div>
+                                {
+                                    productData.map( ( product, index )  => {
+                                        return (     
+                                            <div key={ index } className=" flex flex-col m-10">
+                                                <span className=""><span className=""/><span className="">{product.title}</span></span>
+                                                <span className="">{product.categories}</span>
+                                                <span className="">{product.tags}</span>
+                                                <span className="">{product.description}</span>
+                                                <span className="">{product.submission_date}</span>
+
+
+                                                <span className=""> </span>
+                                                <span className=""> </span>
+
+
+                                                
+
+
+                                                 
+
+                                                
+                                                }
+                                                }
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    }
+                </div>       
         </div>
+        </>
     )
 }
 export default SingleProduct
