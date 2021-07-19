bc518dc71df5028abecc6726257a1d0bab461c83
dineshmhr <dinesh.mhr2054@gmail.com>
2021-07-20 00:11:03 +0545

    updates

--- frontend/src/components/admin/pages/edit-page/editBid.js
+++ frontend/src/components/admin/pages/edit-page/editBid.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import AdminMainNavigation from '../../navigation/AdminMainNavigation'
 
 const AdminEditBid = () => {
     const { id } = useParams()
+
+    useEffect(() => {
+
+    }, [])
 
     return (
         <div id="auction-web-admin" className="content-wrap">
