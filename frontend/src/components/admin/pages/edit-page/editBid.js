93b10ee69a4507f662d245ee5024b15301044060
nehashakya555 <nehashakya555@gmail.com>
2021-08-01 23:54:17 +0545

    fix update

--- frontend/src/components/admin/pages/edit-page/editBid.js
+++ frontend/src/components/admin/pages/edit-page/editBid.js
@@ -101,17 +101,8 @@ const AdminEditBid = () => {
                                             </div>    
                                             </div>
                                         )
-=======
-                                    <div key={ index } className="admin-content-table-row grid grid-cols-6 flex flex-row text-sm">
-                                    <span className="py-2 px-6">{bid.bid_id }</span>
-                                    <span className="py-2 px-6">{bid.title}</span>
-                                    <span className="py-2 px-6">{bid.product_id}</span>
-                                    <span className="py-2 px-6">{bid.initial_bid}</span>
-                                    <span className="py-2 px-6">{bid.max_bid}</span>
-                                               <span className="py-2 px-6">{bid.deadline_date}</span>
-                            </div>
-                        )
->>>>>>> a2c22f93750a337cacf0087be8581ee17f05f99a
+
+                        
                     })
                 }
             </div>
