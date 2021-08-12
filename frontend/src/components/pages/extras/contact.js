1fa98fd02dc8e237e221a063fbedd558d922802f
nehashakya555 <nehashakya555@gmail.com>
2021-08-12 13:03:38 +0545

    fgr

--- frontend/src/components/pages/extras/contact.js
+++ frontend/src/components/pages/extras/contact.js
@@ -7,8 +7,8 @@ const axios = require('axios')
 const Contact = () => {
     const [ email, setEmail ] = useState({ value: ''});
     const [ name, setName ] = useState({ value: ''});
-    const [ feedback, setFeedback ] = useState({value:''});
-    const [ feedBackText, setFeedBackText ] = useState('Send Message');
+    const [ message, setmessage ] = useState({value:''});
+    const [ messageText, setmessageText ] = useState('Send Message');
  
     const validateEmail = () => {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -37,12 +37,12 @@ const Contact = () => {
         return false
     }
 
-    // validate feedback field
-    const validateFeedback = () => {
-        if( feedback.value === '' ) {
-            feedback.error = true;
-            feedback.errorMessage = "Feedback must not be empty";
-            setFeedback( JSON.parse(JSON.stringify( feedback )) )
+    // validate message field
+    const validatemessage = () => {
+        if( message.value === '' ) {
+            message.error = true;
+            message.errorMessage = "message must not be empty";
+            setmessage( JSON.parse(JSON.stringify( message )) )
         } else {
             return true
         }
@@ -51,23 +51,23 @@ const Contact = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        if( validateEmail() && validateName() && validateFeedback() ) {
-            setFeedBackText( "Submitting feedback" )
-            axios.post('/edit-table/edit-feedback.php', {
+        if( validateEmail() && validateName() && validatemessage() ) {
+            setmessageText( "Submitting message" )
+            axios.post('/edit-table/edit-message.php', {
                 name: name.value,
                 email: email.value,
-                feedback: feedback.value
+                message: message.value
             })
             .then(function (response) {
                 if( response.data.status ) {
                     setEmail({value:''})
                     setName({value:''})
-                    setFeedback({value:''})
-                    setFeedBackText( 'Send Feedback' )
+                    setmessage({value:''})
+                    setmessageText( 'Send message' )
                 }
             })
             .catch(function (error) {
-                setFeedBackText( 'Send Feedback' )
+                setmessageText( 'Send message' )
             });
         }
     }
@@ -95,7 +95,7 @@ const Contact = () => {
                                             }
                                         </div>
                                         <div className="md:w-full">
-                                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-feedback" type="text" onChange={ (e) => setEmail({ value: e.target.value }) } placeholder="Your Email address" value={email.value}/>
+                                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-message" type="text" onChange={ (e) => setEmail({ value: e.target.value }) } placeholder="Your Email address" value={email.value}/>
                                         </div>
                                     </div>
                                     <div className="md:flex md:items-center mb-6">
@@ -110,29 +110,29 @@ const Contact = () => {
                                             }
                                         </div>
                                         <div className="md:w-full">
-                                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-feedback" type="text" onChange={ (e) => setName({ value: e.target.value }) } placeholder="Your name" value={name.value}/>
+                                            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-message" type="text" onChange={ (e) => setName({ value: e.target.value }) } placeholder="Your name" value={name.value}/>
                                         </div>
                                     </div>
                                     <div className="md:flex">
                                         <div className="md:w-1/3">
-                                            <label className=" mb-6 mt-2 block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-feedback">
+                                            <label className=" mb-6 mt-2 block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-message">
                                                 Message
                                             </label>
-                                            { feedback.error &&
+                                            { message.error &&
                                                 <div className="aweb-red-note">
-                                                    { feedback.errorMessage }
+                                                    { message.errorMessage }
                                                 </div>
                                             }
                                         </div>
                                         <div className="md:w-full mt-2">
-                                            <textarea className="h-64 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-feedback" type="text" onChange={ (e) => setFeedback({ value: e.target.value }) } placeholder="Your Message" value={feedback.value}/>
+                                            <textarea className="h-64 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-message" type="text" onChange={ (e) => setmessage({ value: e.target.value }) } placeholder="Your Message" value={message.value}/>
                                         </div>
                                     </div>
                                     
                                     <div className="md:flex md:items-center">
                                         <div className="md:w-full mt-4">
                                             <button className=" ml-16 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" onClick = { (e) => onSubmit(e) }>
-                                                { feedBackText }
+                                                { messageText }
                                             </button>
                                         </div>
                                     </div>
